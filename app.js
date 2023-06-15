e01e9d078f99ffecd8ae812d2de4de0c90453a6a
Eze Onyebuchi <eze4acme@gmail.com>
2023-06-15 16:06:51 +0100

    Blog Post With Nodejs
    Creating a blog post with nodejs and mongoDB and express with ejs.

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+const PORT = process.env.PORT || 3000
+app.get('/', (req, res)=>{
+    res.render('index', {title: 'HOME'})
+})
+
+app.get('/about', (req, res) =>{
+    res.render('about', {title: 'About'})
+})
+app.get('/blogs/create', (req, res) =>{
+    res.render('create', {title: 'Create A New Blog'})
+})
+app.use((req, res) =>{
+    res.render('404', {title: '404'})
+})
+
+app.listen(PORT, console.log(`Eze listen on port ${PORT}`))
+
