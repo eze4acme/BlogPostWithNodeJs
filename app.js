f30b0987ee2b09d6e44388b0ff3567005ff00a16
Eze Onyebuchi <eze4acme@gmail.com>
2023-06-16 14:43:09 +0100

    updating css file
    updating css file

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const blogs = require('./data/data.js')
-const app = express()
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
 const PORT = process.env.PORT || 3000
+const app = express()
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res)=>{
 
     res.render('index', {title: 'HOME', blogs})
